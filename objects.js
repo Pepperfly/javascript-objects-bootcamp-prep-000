1187d0d68a3ee424d43e87e84ea33c3c176bcaa9
Jeff Carter <jeff@pepperfly.com>
2018-09-17 16:09:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,7 +4,8 @@ var playlists = {};
 //
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  
+  playlist[artistName] = songTitle;
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
